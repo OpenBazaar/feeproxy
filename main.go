27bbd551e869f34c6c66a1030df17ee2814cdb4a
Tyler Smith <tylersmith.me@gmail.com>
2019-04-29 15:55:47 -0700

    BUGFIX: Use updated source API endpoint.

--- main.go
+++ main.go
@@ -6,72 +6,38 @@ import (
 	"time"
 )
 
-const api string = "https://bitcoinfees.21.co/api/v1/fees/list"
+const api = "https://bitcoinfees.earn.com/api/v1/fees/recommended"
 
-type Fees struct {
-	Fees []FeeLevel `json:"fees"`
+type sourceData struct {
+	Priority int `json:"fastestFee"`
+	Normal   int `json:"halfHourFee"`
+	Economic int `json:"hourFee"`
 }
 
-type FeeLevel struct {
-	MinFee     int `json:"minFee"`
-	MaxFee     int `json:"maxFee"`
-	DayCount   int `json:"dayCount"`
-	MemCount   int `json:"memCount"`
-	MinDelay   int `json:"minDelay"`
-	MaxDelay   int `json:"maxDelay"`
-	MaxMinutes int `json:"maxMinutes"`
-}
-
-type FeeCache struct {
+type responseData struct {
 	Priority int `json:"priority"`
 	Normal   int `json:"normal"`
 	Economic int `json:"economic"`
 }
 
-var httpClient http.Client = http.Client{Timeout: time.Second * 10}
+var httpClient http.Client = http.Client{Timeout: time.Second * 30}
 
 func Query() ([]byte, error) {
 	resp, err := httpClient.Get(api)
 	if err != nil {
 		return nil, err
 	}
-	decoder := json.NewDecoder(resp.Body)
-	fees := new(Fees)
-	err = decoder.Decode(fees)
+
+	feeData := &sourceData{}
+	err = json.NewDecoder(resp.Body).Decode(feeData)
 	if err != nil {
 		return nil, err
 	}
-	defer resp.Body.Close()
+	resp.Body.Close()
 
-	low := FeeLevel{MaxDelay: 0}
-	medium := FeeLevel{MaxDelay: 0}
-	high := FeeLevel{MaxDelay: 10}
-	for _, fl := range fees.Fees {
-		if (fl.MaxDelay > low.MaxDelay && fl.MaxDelay <= 6) || (fl.MaxDelay == low.MaxDelay && fl.MaxFee < low.MaxFee) {
-			low = fl
-		}
-		if (fl.MaxDelay > medium.MaxDelay && fl.MaxDelay <= 3) || (fl.MaxDelay == medium.MaxDelay && fl.MaxFee < medium.MaxFee) {
-			medium = fl
-		}
-		if (fl.MaxDelay < high.MaxDelay) || (fl.MaxDelay == high.MaxDelay && fl.MaxFee < high.MaxFee) {
-			high = fl
-		}
-	}
-	feeCache := FeeCache{
-		Priority: (high.MaxFee + high.MinFee) / 2,
-		Normal:   (medium.MaxFee + medium.MinFee) / 2,
-		Economic: (low.MaxFee + low.MinFee) / 2,
-	}
-
-	if feeCache.Priority == 0 {
-		feeCache.Priority = 1
-	}
-	if feeCache.Normal == 0 {
-		feeCache.Normal = 1
-	}
-	if feeCache.Economic == 0 {
-		feeCache.Economic = 1
-	}
-
-	return json.Marshal(&feeCache)
+	return json.Marshal(&responseData{
+		Priority: feeData.Priority,
+		Normal:   feeData.Normal,
+		Economic: feeData.Economic,
+	})
 }
